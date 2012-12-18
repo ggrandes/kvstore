41e59c5a362a2f3de8af6236152d7678d36d27b5
ggrandes <guillermo.grandes@gmail.com>
2012-12-18 02:56:37 +0100

    usage example

---
+++ src/main/java/org/kvstore/Example.java
@@ -0,0 +1,67 @@
+/*
+ *  Licensed under the Apache License, Version 2.0 (the "License");
+ *  you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ *
+ */
+package org.kvstore;
+import java.util.Iterator;
+
+import org.kvstore.holders.IntHolder;
+import org.kvstore.structures.btree.BplusTree.TreeEntry;
+import org.kvstore.structures.btree.BplusTreeFile;
+
+/**
+ * Code example 
+ *
+ * @author Guillermo Grandes / guillermo.grandes[at]gmail.com
+ */
+public class Example {
+	private static final String btreeFile = "/tmp/test";
+	//
+	public static void main(final String[] args) throws Exception {
+		final int[] keys = new int[] {
+				5, 7, -11, 111, 0
+		};
+		//
+		BplusTreeFile<IntHolder, IntHolder> tree;
+		tree = new BplusTreeFile<IntHolder, IntHolder>(
+			true, 512, 
+			IntHolder.class, IntHolder.class, 
+			btreeFile
+		);
+		//
+		tree.clear();
+		// ============== INSERT
+		for (int i = 0; i < keys.length; i++) {
+			final IntHolder key = IntHolder.valueOf(keys[i]);
+			final IntHolder value = IntHolder.valueOf(i);
+			tree.put(key, value);
+		}
+		tree.sync();
+		// ============== GET
+		System.out.println("tree.get(7)=" + tree.get(IntHolder.valueOf(7)));
+		// ============== REMOVE
+		tree.remove(IntHolder.valueOf(7));
+		// ============== ITERATOR
+		for (Iterator<TreeEntry<IntHolder, IntHolder>> i = tree.iterator(); i.hasNext();) {
+			TreeEntry<IntHolder, IntHolder> e = i.next();
+			System.out.println("Key=" + e.getKey() + " Value=" + e.getValue());
+		}
+		// ============== FIRST / LAST
+		System.out.println("tree.firstKey()=" + tree.firstKey());
+		System.out.println("tree.lastKey()=" + tree.lastKey());
+		//
+		tree.sync();
+		tree.close();
+	}
+
+}
