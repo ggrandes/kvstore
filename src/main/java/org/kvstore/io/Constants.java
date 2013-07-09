0388d3ffd9b59ff18b4c6cc266b376723ae9271f
ggrandes <guillermo.grandes@gmail.com>
2013-07-09 21:17:12 +0200

    System property for disable locking in org.kvstore.io.FileBlockStore

---
+++ src/main/java/org/kvstore/io/Constants.java
@@ -0,0 +1,8 @@
+package org.kvstore.io;
+
+public class Constants {
+	/**
+	 * System property name for global disable of FileChannel Locking
+	 */
+	public static final String PROP_IO_LOCKING = "org.kvstore.io.DisableLocking";
+}
