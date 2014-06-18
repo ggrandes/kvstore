3736fa0a1b7bb1fe1759ed859015744dfde6da52
ggrandes <guillermo.grandes@gmail.com>
2014-06-18 19:35:26 +0200

    changed SystemProperty name

--- src/main/java/org/javastack/kvstore/io/Constants.java
+++ src/main/java/org/javastack/kvstore/io/Constants.java
@@ -4,5 +4,5 @@ public class Constants {
 	/**
 	 * System property name for global disable of FileChannel Locking
 	 */
-	public static final String PROP_IO_LOCKING = "org.kvstore.io.DisableLocking";
+	public static final String PROP_IO_LOCKING = Constants.class.getPackage().getName() + ".DisableLocking";
 }
